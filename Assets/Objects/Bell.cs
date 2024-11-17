ea72ac7cf4b251e1cd980a7a9b460bd73f3d3c80
Jan Pospíšil <pospij52@fit.cvut.cz>
2024-11-17 12:44:42 +0100

    Bell and Goat

--- Assets/Objects/Bell.cs
+++ Assets/Objects/Bell.cs
@@ -6,10 +6,34 @@ public class Bell : MonoBehaviour
 {
     [SerializeField] private AudioSource source;
     [SerializeField] private GameObject soldier;
+    [SerializeField] private GameObject helper;
+    [SerializeField] private LayerMask bearLayer;
 
     public void Ring()
     {
         source.Play();
         Instantiate(soldier);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && helper.activeInHierarchy)
+            Ring();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((bearLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            helper.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if ((bearLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            helper.SetActive(false);
+        }
     }
 }
