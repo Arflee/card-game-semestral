f6570d5344363f3551acf7363278314d163b2b89
sushimat <sushimat@fit.cvut.cz>
2025-02-09 13:12:49 +0100

    Fix load bugs

--- Assets/CombatSystem/Scripts/RewardPanel.cs
+++ Assets/CombatSystem/Scripts/RewardPanel.cs
@@ -49,6 +49,7 @@ public class RewardPanel : MonoBehaviour
             var cardSlot = Instantiate(cardSlotPrefab, panel.transform);
             var card = cardSlot.GetComponentInChildren<Card>();
             card.Initialize(combatCard, null, panel.transform.position);
+            card.DisableCard();
 
             card.CardVisual.LocalCanvas.overrideSorting = true;
             card.CardVisual.LocalCanvas.sortingLayerName = "Foreground";
